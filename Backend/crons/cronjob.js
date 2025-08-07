e934d56d850155380e96ae4a64315c51a0eb0f48
Aariyan S <146668583+Aariyan007@users.noreply.github.com>
2025-08-07 12:31:12 +0530

    Update special characters of cronjob.js

--- Backend/crons/cronjob.js
+++ Backend/crons/cronjob.js
@@ -4,19 +4,19 @@ const sendEmailsToUsers = require('../mailSender/sendMail');
 
 cron.schedule('0 * * * *', async () => {
     const hour = new Date().getHours();
-    console.log(`⏰ Checking for scheduled emails at ${hour}:00`);
+    console.log(`Checking for scheduled emails at ${hour}:00`);
     await sendEmailsToUsers();
 });
 
 
 cron.schedule('0 9 * * *', async () => {
-    console.log('🌅 Running 9 AM job batch');
+    console.log(' Running 9 AM job batch');
     await sendEmailsToUsers();
 });
 
 cron.schedule('0 21 * * *', async () => {
-    console.log('🌙 Running 9 PM job batch');
+    console.log('Running 9 PM job batch');
     await sendEmailsToUsers();
 });
 
-console.log('📅 Cron jobs scheduled successfully!');
+console.log('Cron jobs scheduled successfully!');
